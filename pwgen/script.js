31a262b928b553cc5437ee358093df7942352012
Serhii Tsynailo <serhii.tsynailo@gmail.com>
2024-07-21 13:03:47 +0300

    added pwgen

---
+++ pwgen/script.js
@@ -0,0 +1,37 @@
+function toggleVisibility(e) {
+  const visivilityIcon = e.parentElement.querySelector("i");
+  const textInput = e.parentElement.parentElement.querySelector("input");
+  if (textInput.type === "password") {
+    textInput.type = "text";
+    visivilityIcon.classList.remove("fa-eye");
+    visivilityIcon.classList.add("fa-eye-slash");
+  } else {
+    textInput.type = "password";
+    visivilityIcon.classList.remove("fa-eye-slash");
+    visivilityIcon.classList.add("fa-eye");
+  }
+}
+
+function stringToBase64(bytes) {
+  const binString = String.fromCodePoint(...bytes);
+  return btoa(binString);
+}
+
+async function sha512(str) {
+  const buf = await crypto.subtle.digest(
+    "SHA-512",
+    new TextEncoder().encode(str)
+  );
+  return Array.prototype.map
+    .call(new Uint8Array(buf), (x) => ("00" + x.toString(16)).slice(-2))
+    .join("");
+}
+
+async function generate() {
+  const pp = document.querySelector("#passphrase");
+  const p = document.querySelector("#pin");
+  if (pp.value) {
+    let s = (await sha512(pp.value + p.value)).slice(0, 12);
+    navigator.clipboard.writeText(stringToBase64(new TextEncoder().encode(s)));
+  }
+}
